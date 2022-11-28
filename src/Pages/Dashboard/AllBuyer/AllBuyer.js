2c786f7638725db226a2fe6f8d17b8e0e8b3d965
Amit7366 <40818508+Amit7366@users.noreply.github.com>
2022-11-28 18:01:11 +0600

    delete buyer

--- src/Pages/Dashboard/AllBuyer/AllBuyer.js
+++ src/Pages/Dashboard/AllBuyer/AllBuyer.js
@@ -1,9 +1,72 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import toast from 'react-hot-toast';
 
 const AllBuyer = () => {
+    const { data: allbuyer = [], refetch, isLoading } = useQuery({
+        queryKey: ['allbuyer'],
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:5000/buyer`);
+            const data = await res.json();
+            return data
+        }
+    });
+
+    const handleDelete = (id) =>{
+        fetch(`http://localhost:5000/buyer/${id}`, {
+            method: 'DELETE', 
+            headers: {
+                authorization: `bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(data.deletedCount > 0){
+                refetch();
+                toast.success(`Buyer deleted successfully`)
+            }
+        })
+    }
+
     return (
         <div>
             <h1>All Buyer</h1>
+            <table className="table w-full">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {allbuyer.map((seller, _idx) => (
+            <tr key={seller._id}>
+              <th>{_idx + 1}</th>
+              <td>{seller.name}</td>
+              <td>{seller.email}</td>
+              <td>
+                {seller.status === "verified" ? (
+                  <button
+                    className="btn btn-sm btn-success text-white"
+                    disabled
+                  >
+                    {seller.status}
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => handleDelete(seller._id)}
+                    className="btn btn-sm btn-warning text-white"
+                  >
+                    Delete
+                  </button>
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
         </div>
     );
 };
--- src/Pages/Dashboard/AllSeller/AllSeller.js
+++ src/Pages/Dashboard/AllSeller/AllSeller.js
@@ -1,44 +1,45 @@
-import { useQuery } from '@tanstack/react-query';
-import React from 'react';
-import toast from 'react-hot-toast';
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import toast from "react-hot-toast";
 
 const AllSeller = () => {
-    const { data: allSeller = [], refetch, isLoading } = useQuery({
-        queryKey: ['allSeller'],
-        queryFn: async () => {
-            const res = await fetch(`http://localhost:5000/seller`);
-            const data = await res.json();
-            return data
+  const {
+    data: allSeller = [],
+    refetch,
+    isLoading,
+  } = useQuery({
+    queryKey: ["allSeller"],
+    queryFn: async () => {
+      const res = await fetch(`http://localhost:5000/seller`);
+      const data = await res.json();
+      return data;
+    },
+  });
+
+  const handleVerifiation = (id) => {
+    fetch(`http://localhost:5000/seller/${id}`, {
+      method: "PUT",
+      headers: {},
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount > 0) {
+          toast.success("Seller successfuly Verified.");
+          refetch();
         }
-    });
+      });
+  };
 
-    const handleVerifiation = (id) =>{
-        fetch(`http://localhost:5000/seller/${id}`,{
-            method: 'PUT',
-            headers: {
-
-            }
-        })
-        .then(res => res.json())
-        .then(data =>{
-            if(data.modifiedCount > 0){
-                toast.success('Seller successfuly Verified.')
-                refetch();
-            }
-        })
-    }
-
-    return (
-        <div className="overflow-x-auto">
-            <h1>All Seller</h1>
-            <table className="table w-full">
+  return (
+    <div className="overflow-x-auto">
+      <h1>All Seller</h1>
+      <table className="table w-full">
         <thead>
           <tr>
             <th>#</th>
             <th>Name</th>
             <th>Email</th>
             <th>Status</th>
-            
           </tr>
         </thead>
         <tbody>
@@ -47,17 +48,29 @@ const AllSeller = () => {
               <th>{_idx + 1}</th>
               <td>{seller.name}</td>
               <td>{seller.email}</td>
-              <td>{seller.status === 'verified' ? 
-              <button className="btn btn-sm btn-success text-white" disabled>{seller.status}</button> 
-              : 
-              <button onClick={()=>handleVerifiation(seller._id)} className="btn btn-sm btn-warning text-white">Verify</button> }</td>
-              
+              <td>
+                {seller.status === "verified" ? (
+                  <button
+                    className="btn btn-sm btn-success text-white"
+                    disabled
+                  >
+                    {seller.status}
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => handleVerifiation(seller._id)}
+                    className="btn btn-sm btn-warning text-white"
+                  >
+                    Verify
+                  </button>
+                )}
+              </td>
             </tr>
           ))}
         </tbody>
       </table>
-        </div>
-    );
+    </div>
+  );
 };
 
 export default AllSeller;
